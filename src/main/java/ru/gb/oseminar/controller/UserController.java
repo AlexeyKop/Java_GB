939742c25af4c1f00b421b426265c17323678e4e
AlexeyKop <a.kop@bk.ru>
2024-03-28 22:50:10 +0400

    ___

--- src/main/java/ru/gb/oseminar/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.oseminar.controller;
-
-import ru.gb.oseminar.data.User;
-
-import java.time.LocalDate;
-
-public interface UserController<T extends User>{
-    void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth);
-}
