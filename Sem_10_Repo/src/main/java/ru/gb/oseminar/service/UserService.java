8e63886accd9ec76626867cb59e55f7e45fcc1c2
AlexeyKop <a.kop@bk.ru>
2024-03-29 18:04:27 +0400

    разобрали задание семинара 4

--- Sem_10_Repo/src/main/java/ru/gb/oseminar/service/UserService.java
+++ Sem_10_Repo/src/main/java/ru/gb/oseminar/service/UserService.java
@@ -3,7 +3,7 @@ package ru.gb.oseminar.service;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface UserService<T> {
+public interface UserService<T extends User> {
     List<T> getAll();
     void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth);
 }
